41c43b1e0baaa9c180c36bd2c396d6fc4c801776
Segni Girma <segnigirma11@gmail.com>
2025-07-14 07:41:46 +0300

    Created 'Remove Duplicate Letters' by A2SV Hub

---
+++ 14-Jul-2025/Remove Duplicate Letters 376350.py
@@ -0,0 +1,23 @@
+# Problem: Remove Duplicate Letters - https://leetcode.com/problems/remove-duplicate-letters/
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+
+        counter_word = Counter(s)
+        in_stack = 0
+        stack = []
+
+        for char in s:
+            counter_word[char] -= 1
+            bit = 1 << (ord(char) - ord("a"))
+            if in_stack & bit:
+                continue
+            while stack and char < stack[-1] and counter_word[stack[-1]] > 0:
+                popped_char = stack.pop()
+                in_stack ^= 1 << (ord(popped_char) - ord("a"))
+                
+            
+            stack.append(char)
+            in_stack |= bit
+        
+        return "".join(stack)
