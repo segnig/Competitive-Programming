c777e570f2e87e605cec2d6cdd5e0cf1a2e144ba
Segni Girma <segnigirma11@gmail.com>
2025-07-18 04:44:52 -0700

    Created 'Robot Collisions' by A2SV Hub

---
+++ 18-Jul-2025/Robot Collisions 377585.py
@@ -0,0 +1,31 @@
+# Problem: Robot Collisions - https://leetcode.com/problems/robot-collisions/
+
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        robots = [[positions[i], healths[i], directions[i], i] for i in range(len(positions))]
+        robots.sort()
+
+        stack = []
+        for robot in robots:
+            if not stack or robot[2] == "R":
+                stack.append(robot)
+            else:
+                online = True
+                while stack and stack[-1][2] != robot[2]:
+                    if stack[-1][1] == robot[1]:
+                        stack.pop()
+                        online = False
+                        break
+                    elif stack[-1][1] > robot[1]:
+                        stack[-1][1] -= 1
+                        online = False
+                        break
+                    else:
+                        stack.pop()
+                        robot[1] -= 1
+                if online:
+                    stack.append(robot)
+        if not stack:
+            return []
+        stack.sort(key=lambda x: x[-1])
+        return [x[1] for x in stack]
