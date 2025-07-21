da8cacc9459e294dd0a7b8d4f849b896f8fb0ced
Segni Girma <segnigirma11@gmail.com>
2025-07-20 22:56:27 -0700

    Created 'Swap Cases' by A2SV Hub

---
+++ 21-Jul-2025/Swap Cases 378293.py
@@ -0,0 +1,9 @@
+# Problem: Swap Cases - https://www.hackerrank.com/challenges/swap-case?isFullScreen=true
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
