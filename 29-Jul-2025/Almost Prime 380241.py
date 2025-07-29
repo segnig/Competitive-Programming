46a0015a066566c5f1836584e8996e20c574955c
Segni Girma <segnigirma11@gmail.com>
2025-07-29 16:34:00 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 29-Jul-2025/Almost Prime 380241.py
@@ -0,0 +1,27 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+num = int(input())
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+for i in range(2, num//2 + 1):
+    if is_prime(i):
+        primes.append(i)
+
+count = 0
+m = len(primes)
+for i in range(2,num+1):
+    div = 0
+    for j in range(m):
+        if i % primes[j] == 0:
+            div += 1
+    if div == 2:
+        count += 1
+print(count)
