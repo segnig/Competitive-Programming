acdb467726172edbec2f6ae9ee37f9886e40daa0
Segni Girma <segnigirma11@gmail.com>
2025-08-05 16:10:55 +0300

    Created 'Continuous Subarray Sum' by A2SV Hub

---
+++ 05-Aug-2025/Continuous Subarray Sum 382046.py
@@ -0,0 +1,18 @@
+# Problem: Continuous Subarray Sum - https://leetcode.com/problems/continuous-subarray-sum/
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pre_sum = []
+        hash_map = {0:-1}
+
+        for i, num in enumerate(nums):
+            if not pre_sum:
+                pre_sum.append(num % k)
+            else:
+                pre_sum.append((pre_sum[-1] + num )% k)
+            if pre_sum[-1] not in hash_map:
+                hash_map[pre_sum[-1]] = i
+            elif i - hash_map[pre_sum[-1]] > 1:
+                return True
+
+        return False
