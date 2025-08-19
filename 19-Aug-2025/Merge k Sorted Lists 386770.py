4a333aa7e45eb0dd9e524c832f9e2319e194ef51
Segni Girma <segnigirma11@gmail.com>
2025-08-19 10:49:37 +0300

    Created 'Merge k Sorted Lists' by A2SV Hub

---
+++ 19-Aug-2025/Merge k Sorted Lists 386770.py
@@ -0,0 +1,29 @@
+# Problem: Merge k Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        queue = [(head.val, i) for i, head in enumerate(lists) if head]
+        
+        heapify(queue)
+
+        result = ListNode()
+        temp = result
+
+        while queue:
+            val, index = heappop(queue)
+            node = lists[index]
+            temp.next = ListNode(node.val)
+            if node.next:
+                heappush(queue, (node.next.val, index))
+            if node.next:
+                lists[index] = node.next
+            else:
+                lists[index] = None
+
+            temp = temp.next
+        return result.next
