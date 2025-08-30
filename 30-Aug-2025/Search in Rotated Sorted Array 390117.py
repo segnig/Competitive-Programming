5e991412aef5395eddff1255b2581901515dc1f1
Segni Girma <segnigirma11@gmail.com>
2025-08-30 14:13:36 +0300

    Created 'Search in Rotated Sorted Array' by A2SV Hub

---
+++ 30-Aug-2025/Search in Rotated Sorted Array 390117.py
@@ -0,0 +1,20 @@
+# Problem: Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (right + left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[mid] < target or nums[left] > target:
+                    left = mid + 1
+                else:
+                    right = mid - 1 
+            else:
+                if nums[mid] > target or nums[right] < target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
