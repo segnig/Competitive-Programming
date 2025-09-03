130d783b491fd535a6998b76b74b4c27d5e5b597
Segni Girma <segnigirma11@gmail.com>
2025-09-03 18:34:12 +0300

    Created 'Sum of Square Numbers' by A2SV Hub

---
+++ 03-Sep-2025/Sum of Square Numbers 391069.py
@@ -0,0 +1,17 @@
+# Problem: Sum of Square Numbers - https://leetcode.com/problems/sum-of-square-numbers/
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left, right = 0, int(sqrt(c) + 1)
+
+        while left <= right:
+            hyp = left * left + right * right
+
+            if hyp == c:
+                return True
+            elif hyp < c:
+                left += 1
+            else:
+                right -= 1
+        
+        return False
