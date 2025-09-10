2fdfe9046b37d69f907d2bea9b3325c1f162ce77
Segni Girma <segnigirma11@gmail.com>
2025-09-10 19:46:03 +0300

    Created 'Separate Black and white balls' by A2SV Hub

---
+++ 10-Sep-2025/Separate Black and white balls 392448.py
@@ -0,0 +1,11 @@
+# Problem: Separate Black and white balls - https://leetcode.com/problems/separate-black-and-white-balls/
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        l = 0
+        res = 0
+        for right in range(len(s)):
+            if s[right] == "0":
+                res += right - l
+                l += 1
+        return res   
