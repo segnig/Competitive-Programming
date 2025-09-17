35237f707c5d4a31f8f692c8d979ce6ca3f25784
Segni Girma <segnigirma11@gmail.com>
2025-09-17 11:50:55 +0300

    Created 'Relative Ranks' by A2SV Hub

---
+++ 17-Sep-2025/Relative Ranks 393653.py
@@ -0,0 +1,22 @@
+# Problem: Relative Ranks - https://leetcode.com/problems/relative-ranks/description/
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sc = sorted(score, reverse=True)
+        rank = {val: index + 1 for index, val in enumerate(sc)}
+
+        result = []
+        for val in score:
+            result.append(self.ranker(rank[val]))
+
+        return result
+
+    
+    def ranker(self, i):
+        if i == 1:
+            return "Gold Medal"
+        if i == 2:
+            return "Silver Medal"
+        if i == 3:
+            return "Bronze Medal"
+        return str(i)
