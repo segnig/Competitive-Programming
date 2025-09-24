ddb9931597ca77b354aa2823f2cefe64ae8c5904
Segni Girma <segnigirma11@gmail.com>
2025-09-24 16:41:55 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 24-Sep-2025/Fibonacci Number 395685.py
@@ -0,0 +1,13 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {}
+        def fn(n):
+            if n < 2:
+                return n
+            if n not in memo:
+                memo[n] =  fn(n-1) + fn(n-2)
+            return memo[n]
+        
+        return fn(n)
