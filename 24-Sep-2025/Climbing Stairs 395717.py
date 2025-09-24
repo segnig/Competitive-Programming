efda258357813f3f0db97eee547e8167c22ace69
Segni Girma <segnigirma11@gmail.com>
2025-09-24 17:19:31 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 24-Sep-2025/Climbing Stairs 395717.py
@@ -0,0 +1,16 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        
+        def rec(n):
+            if n <= 2:
+                return n
+            if n not in memo:
+                memo[n] = rec(n-1) + rec(n-2)
+        
+            return memo[n]
+
+        return rec(n)
+        
