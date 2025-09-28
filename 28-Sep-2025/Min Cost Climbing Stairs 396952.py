abf62cc1caa3425642b18a0a495ab008d4255c55
Segni Girma <segnigirma11@gmail.com>
2025-09-28 08:57:20 +0300

    Created 'Min Cost Climbing Stairs' by A2SV Hub

---
+++ 28-Sep-2025/Min Cost Climbing Stairs 396952.py
@@ -0,0 +1,20 @@
+# Problem: Min Cost Climbing Stairs - https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        memo = {}
+
+        def dp(ind):
+            if ind < 0:
+                return 0
+            if ind not in memo:
+                one = cost[ind] + dp(ind - 1)
+                two = cost[ind] + dp(ind - 2)
+                memo[ind] = min(one, two)
+
+            return memo[ind]
+
+        return dp(len(cost) - 1)
+
+        
