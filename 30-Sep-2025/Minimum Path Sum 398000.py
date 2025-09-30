6768ab303cd6fea1f8f3e8a456e4e69956e82553
Segni Girma <segnigirma11@gmail.com>
2025-09-30 16:44:06 +0300

    Created 'Minimum Path Sum' by A2SV Hub

---
+++ 30-Sep-2025/Minimum Path Sum 398000.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Path Sum - https://leetcode.com/problems/minimum-path-sum/description/
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        @cache
+        def dp(i, j):
+            if i == 0 and j == 0:
+                return grid[i][j]
+            if i < 0 or j < 0:
+                return inf
+            return min(grid[i][j] + dp(i -1, j), grid[i][j] + dp(i, j - 1))
+
+        return dp(len(grid) - 1, len(grid[0]) - 1)
