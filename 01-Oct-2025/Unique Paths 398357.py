a2a7e15ca5de421d6db4c05546ab470f3acba59a
Segni Girma <segnigirma11@gmail.com>
2025-10-01 10:46:09 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 01-Oct-2025/Unique Paths 398357.py
@@ -0,0 +1,11 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp = [[1] *m for _ in range(n)]
+
+        for row in range(1, n):
+            for col in range(1, m):
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+        return dp[n -1][m-1]
