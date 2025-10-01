d2ae95f480ae6e7086057b33e2a566a716f79a16
Segni Girma <segnigirma11@gmail.com>
2025-10-01 16:27:39 +0300

    Created 'Perfect Squares' by A2SV Hub

---
+++ 01-Oct-2025/Perfect Squares 398601.py
@@ -0,0 +1,22 @@
+# Problem: Perfect Squares - https://leetcode.com/problems/perfect-squares/
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        temp = []
+
+        for i in range(1, int(sqrt(n))+1):
+            temp.append(i*i)
+        @cache
+        def dp(rem):
+            if rem == 0:
+                return 0
+            if rem < 0:
+                return inf
+            
+            result = inf
+            for num in temp:
+                result = min(result, 1 + dp(rem - num))
+
+            return result
+        
+        return dp(n)
