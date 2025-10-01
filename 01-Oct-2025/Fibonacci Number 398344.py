9f5adea56899ede35bd7d09c1a43af53ce81d126
Segni Girma <segnigirma11@gmail.com>
2025-10-01 10:35:28 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 01-Oct-2025/Fibonacci Number 398344.py
@@ -0,0 +1,11 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        first, second = 0, 1
+
+        for _ in range(2, n):
+            second, first = second + first, second
+        return second + first
