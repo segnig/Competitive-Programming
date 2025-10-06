0752ed64ad3b7eb3e4451e303c4dbb94a5645922
Segni Girma <segnigirma11@gmail.com>
2025-10-06 11:01:05 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 06-Oct-2025/Almost Prime 400181.py
@@ -0,0 +1,26 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def prime_factorization(n):
+    d = 2
+
+    factorization = set()
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factorization.add(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factorization.add(n)
+
+    return factorization
+
+num = int(input())
+
+count = 0
+
+for i in range(2, num+1):
+    if len(prime_factorization(i)) == 2:
+        count += 1
+
+print(count)
