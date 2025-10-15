59156157beb70c4df5116e36a89cd58643accba2
Segni Girma <segnigirma11@gmail.com>
2025-10-15 15:15:09 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 15-Oct-2025/Valid Anagram 403300.py
@@ -0,0 +1,13 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_counter = Counter(s)
+        t_counter = Counter(t)
+        for char in s_counter:
+            if s_counter[char] != t_counter[char]:
+                return False
+        return True
