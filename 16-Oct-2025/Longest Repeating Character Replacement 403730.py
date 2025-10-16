dd228757e29cde1d460a3230b04d9fd86c8cb32e
Segni Girma <segnigirma11@gmail.com>
2025-10-16 18:11:45 +0300

    Created 'Longest Repeating Character Replacement' by A2SV Hub

---
+++ 16-Oct-2025/Longest Repeating Character Replacement 403730.py
@@ -0,0 +1,21 @@
+# Problem: Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counter = defaultdict(int)
+
+        result = left = most_freq = 0
+
+        for right in range(len(s)):
+            counter[s[right]] += 1
+            most_freq = max(counter[s[right]], most_freq)
+
+            while right - left - most_freq + 1> k:
+                counter[s[left]] -= 1
+                left += 1
+            
+            result = max(result, right - left + 1)
+        
+        return max(result, len(s) - left)
+
+            
