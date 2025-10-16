514831d523a6e4eb4b75f0d9af26656fd43a3a5c
Segni Girma <segnigirma11@gmail.com>
2025-10-16 15:53:47 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 16-Oct-2025/Group Anagrams 403696.py
@@ -0,0 +1,15 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        store = defaultdict(list)
+
+        for word in strs:
+            counter = [0]*26
+            for char in word:
+                idx = ord(char) - ord("a")
+                counter[idx] += 1
+
+            store[tuple(counter)].append(word)
+
+        return list(store.values())
