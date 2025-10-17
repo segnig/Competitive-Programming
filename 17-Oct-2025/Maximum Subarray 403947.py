ac15bb724505c92a0d8554cb1201631367861491
Segni Girma <segnigirma11@gmail.com>
2025-10-17 12:45:15 +0300

    Created 'Maximum Subarray' by A2SV Hub

---
+++ 17-Oct-2025/Maximum Subarray 403947.py
@@ -0,0 +1,14 @@
+# Problem: Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        result = max(nums)
+        prefix = 0
+        for num in nums:
+            prefix += num
+            result = max(result, prefix)
+            
+            if prefix < 0:
+                prefix = 0
+
+        return result
