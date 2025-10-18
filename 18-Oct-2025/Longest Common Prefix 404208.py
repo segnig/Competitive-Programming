a89de2dda155d4e7b7038cf4060ba52870edfdc8
Segni Girma <segnigirma11@gmail.com>
2025-10-18 13:03:24 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 18-Oct-2025/Longest Common Prefix 404208.py
@@ -0,0 +1,15 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+        for word in strs[1:]:
+            temp = ""
+
+            for i in range(len(word)):
+                if i == len(prefix) or prefix[i] != word[i]:
+                    break
+                temp += prefix[i]
+            prefix = temp
+        
+        return prefix   
