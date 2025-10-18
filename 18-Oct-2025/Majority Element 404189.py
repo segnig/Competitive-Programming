6f311a2775b5864b87a7ed36aa53ed19c0f5aee1
Segni Girma <segnigirma11@gmail.com>
2025-10-18 07:53:08 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 18-Oct-2025/Majority Element 404189.py
@@ -0,0 +1,17 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans = None
+        counter = 0
+
+        for num in nums:
+            if counter == 0:
+                ans = num
+            if ans == num:
+                counter += 1
+            else:
+                counter -= 1
+        
+        return ans
+        
