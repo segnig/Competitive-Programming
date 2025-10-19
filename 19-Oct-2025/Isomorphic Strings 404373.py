07937a930e6380cbefc2b3985d3a75eacfbffc06
Segni Girma <segnigirma11@gmail.com>
2025-10-19 13:07:24 +0300

    Created 'Isomorphic Strings' by A2SV Hub

---
+++ 19-Oct-2025/Isomorphic Strings 404373.py
@@ -0,0 +1,18 @@
+# Problem: Isomorphic Strings - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        replace_s = {}
+        replace_t = {}
+
+
+        for i in range(len(s)):
+            if s[i] not in replace_s:
+                replace_s[s[i]] = t[i]
+            if t[i] not in replace_t:
+                replace_t[t[i]] = s[i]
+            
+            if t[i] != replace_s[s[i]] or s[i] != replace_t[t[i]]:
+                return False
+        
+        return True
