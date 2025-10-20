b0644f98b70a2a442d3765fdf74db962c9b226ee
Segni Girma <segnigirma11@gmail.com>
2025-10-20 15:23:01 +0300

    Created 'Merge Intervals (Optional)' by A2SV Hub

---
+++ 20-Oct-2025/Merge Intervals (Optional) 404588.py
@@ -0,0 +1,14 @@
+# Problem: Merge Intervals (Optional) - https://leetcode.com/problems/merge-intervals/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    
+        intervals.sort()
+
+        result = []
+        for start, end in intervals:
+            if result and result[-1][1] >= start:
+                result[-1][1] = max(result[-1][1], end)
+            else:
+                result.append([start, end])
+        return result
