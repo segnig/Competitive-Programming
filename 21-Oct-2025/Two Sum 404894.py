34c5cf09c2a7932af242a06b96db31617bfdf1df
Segni Girma <segnigirma11@gmail.com>
2025-10-21 18:09:45 +0300

    Created 'Two Sum' by A2SV Hub

---
+++ 21-Oct-2025/Two Sum 404894.py
@@ -0,0 +1,10 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/description
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+
+        for index, num in enumerate(nums):
+            if target - num in store:
+                return [store[target - num], index]
+            store[num] = index
