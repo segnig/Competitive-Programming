9bebfc2e57b36c7cf667c35fcf710c00ee5eecff
Segni Girma <segnigirma11@gmail.com>
2025-10-25 14:42:58 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 25-Oct-2025/Palindrome Number 405674.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # original nums
+        original = x
+        result = 0
+
+        while x > 0:
+            res = x % 10
+            x //= 10
+            result = result * 10 + res
+        
+        return result == original
+        
