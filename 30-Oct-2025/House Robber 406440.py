edcf1f02ed2bf2f39e7795d617a33b14ed888c8c
Segni Girma <segnigirma11@gmail.com>
2025-10-30 14:35:42 +0300

    Created 'House Robber' by A2SV Hub

---
+++ 30-Oct-2025/House Robber 406440.py
@@ -0,0 +1,18 @@
+# Problem: House Robber - https://leetcode.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        memo = {}
+
+        def dp(index):
+            if index >= len(nums):
+                return 0
+            if index not in memo:
+                take = nums[index] + dp(index + 2)
+                not_take = dp(index + 1)
+
+                memo[index] = max(take, not_take)
+            return memo[index]
+
+        return dp(0)
