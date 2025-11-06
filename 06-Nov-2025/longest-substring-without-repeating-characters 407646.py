6e53fd3c0cd3e7cd3be177eb61a70caefeb03a5a
Segni Girma <segnigirma11@gmail.com>
2025-11-06 15:09:04 +0300

    Created 'longest-substring-without-repeating-characters' by A2SV Hub

---
+++ 06-Nov-2025/longest-substring-without-repeating-characters 407646.py
@@ -0,0 +1,17 @@
+# Problem: longest-substring-without-repeating-characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, word: str) -> int:
+        counter = defaultdict(int)
+    
+        left = 0
+        answer = 0
+        
+        for right in range(len(word)):
+            while counter[word[right]] > 0:
+                counter[word[left]] -= 1
+                left += 1
+            counter[word[right]] += 1
+            answer = max(answer, right - left + 1)
+        
+        return answer
