2ce9bc738f17182315804bf7019781614e74b3b3
Segni Girma <segnigirma11@gmail.com>
2025-11-11 17:43:09 +0300

    Created 'Maximum Candies Allocated to K Children' by A2SV Hub

---
+++ 11-Nov-2025/Maximum Candies Allocated to K Children 408382.py
@@ -0,0 +1,23 @@
+# Problem: Maximum Candies Allocated to K Children - https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        left, right = 1, max(candies)
+
+        def checker(x):
+            count = 0
+            for candy in candies:
+                count += candy // x
+            
+            return count >= k
+        
+        while left <= right:
+            mid = (right + left) // 2
+            
+            if checker(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
+        
