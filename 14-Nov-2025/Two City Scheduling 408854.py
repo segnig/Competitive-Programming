e48dc4a2811245e7a4e442dce6da7a615f8e3762
Segni Girma <segnigirma11@gmail.com>
2025-11-14 18:04:28 +0300

    Created 'Two City Scheduling' by A2SV Hub

---
+++ 14-Nov-2025/Two City Scheduling 408854.py
@@ -0,0 +1,13 @@
+# Problem: Two City Scheduling - https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        costs.sort(key =lambda cost: cost[1] - cost[0])
+
+        total_cost = 0
+        for index, _ in enumerate(costs):
+            total_cost += costs[index][index < len(costs) // 2]
+        
+        return total_cost
+
