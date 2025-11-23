efbb55404c29455d1177a6923650bc6a060a86c5
Segni Girma <segnigirma11@gmail.com>
2025-11-23 13:39:34 +0300

    Created 'Dictionary' by A2SV Hub

---
+++ 23-Nov-2025/Dictionary 409619.py
@@ -0,0 +1,13 @@
+# Problem: Dictionary - https://codeforces.com/problemset/problem/1674/B
+
+for _ in range(int(input())):
+    word = input()
+
+    a = ord(word[0]) - ord("a")
+
+    b = ord(word[1]) - ord("a")
+    result = a * 25 + b
+    if word[0] > word[1]:
+        result += 1
+    
+    print(result)
