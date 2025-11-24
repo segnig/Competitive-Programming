c744cd0c69d5656defbd2119337f50e551d56739
Segni Girma <segnigirma11@gmail.com>
2025-11-24 12:35:52 +0300

    Created 'Minimum Integer' by A2SV Hub

---
+++ 24-Nov-2025/Minimum Integer 409680.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+for _ in range(int(input())):
+    l, r, d = map(int, input().split())
+
+    if l > d:
+        print(d)
+    else:
+        m = r % d
+
+        if m == 0:
+            print(r + d)
+        else:
+            print((r // d + 1) * d)
