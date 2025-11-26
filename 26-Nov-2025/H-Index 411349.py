bc1053081ae917b931307f2f88c2165082c4f5fe
Segni Girma <segnigirma11@gmail.com>
2025-11-26 16:05:08 +0300

    Created 'H-Index' by A2SV Hub

---
+++ 26-Nov-2025/H-Index 411349.py
@@ -0,0 +1,13 @@
+# Problem: H-Index - https://leetcode.com/problems/h-index/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+
+        for index, cite in enumerate(citations):
+            if cite >= index + 1:
+                h = index + 1
+
+            else: return h  
+        return h
